d32f283347b1aa3fc09b8e996594df6cfe7c4706
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-02-27 12:08:13 +0200

    2
    2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movie">
+    <BrowserRouter basename="/goit-react-hw-05-movie/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
